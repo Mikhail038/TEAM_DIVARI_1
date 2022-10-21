69cc22e8501a00ac3b8505d03e28dc4a77ed589b
Mikhail <111287669+Mikhail038@users.noreply.github.com>
2022-10-21 13:31:28 +0300

    Added main programm 0.2

--- resistance.cpp
+++ resistance.cpp
@@ -1,5 +1,43 @@
+#include "resistance.h"
+
+
+const int N = 100;
+const double epsilon = 1e-6;
+
 int main ()
 {
 
-  return 0;
+    double voltage[N];
+    double current[N];
+
+    int M = 0;
+
+    while (scanf ("%lg/%lg", &voltage[M], &current[M]) == 2)
+    {
+        M++;
+    }
+
+    double resistance[N];
+
+    for (int i = 0; i < M; i++)
+    {
+        if (current[i] < epsilon)
+        {
+            printf ("Zero division error!\n");
+            return 1;
+        }
+
+        resistance[i] = voltage[i] / current[i];
+    }
+
+    int K = data_select (M, resistance[]);
+
+    double resistance_final = result (K, resistance[]);
+
+    if (check (K, resistance[], resistance_final) != 1)
+    {
+        printf ("Bad data!\n");
+    }
+
+    return 0;
 }
---
+++ resistance.h
@@ -0,0 +1,11 @@
+//MAIN HEADER FILE
+
+int preparation (double resistance[]);//returns M
+
+int data_select (int M, double resistance[]);//returns K
+
+double result (int K, double resistance[]);//returns R
+
+int check (int K, double resistance[], double resistance_final);//returns 1 if result is correct | 0 if incorrect
+
+
