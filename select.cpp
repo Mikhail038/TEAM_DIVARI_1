8d89be22d3910823d8cd23682b6941a2e455411e
Elisey-e <58297256+Elisey-e@users.noreply.github.com>
2022-10-21 13:42:25 +0300

    Add files via upload

---
+++ select.cpp
@@ -0,0 +1,20 @@
+int data_select(int M, double resistance[]){
+    double resistance_average;
+    for (int i = 0; i < M; i++){
+        resistance_average += resistance[i] / M;
+    }
+    int delta = 0;
+    int check = 0;
+    for (int i = 0; check < M; i += 0){
+        if (fabs(fabs(resistance_average - resistance[check]) / resistance_average) > 0.03){
+            check++;
+            delta++;
+        }
+        else{
+            resistance[i] = resistance[check];
+            check++;
+            i++;
+        }
+    }
+    return M - delta;
+}
